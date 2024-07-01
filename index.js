94f9fbff0c40727cb140ffb4c124f34e8ea16b06
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2024-07-01 22:30:15 +0100

    chore: error handling

--- index.js
+++ index.js
@@ -21,46 +21,55 @@ const s3Client = new S3Client({
     forcePathStyle: true, // this allows the sdk follow the endpoint structure and not append the bucket name
   });
 
-const uploadFileToBucket = async ({ bucketName, file }) => {
-    const getFileExt = file.originalname.split('.').pop();
-    
-    const filePath = file.path;
-    const fileContent = readFileSync(filePath);
-    const upload = await s3Client.send(
-      new PutObjectCommand({
-        Bucket: bucketName,
-        Body: fileContent,
-        Key: file.filename + '.' + getFileExt,
-        ACL: 'public-read',
-      }),
-    )
+  const uploadFileToBucket = async ({ bucketName, file }) => {
+    try {
+      const getFileExt = file.originalname.split('.').pop();
   
-    unlinkSync(file.path);
-    return upload;
+      const filePath = file.path;
+      const fileContent = readFileSync(filePath);
+      const upload = await s3Client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Body: fileContent,
+          Key: file.filename + '.' + getFileExt,
+          ACL: 'public-read',
+        }),
+      );
+  
+      unlinkSync(file.path);
+      return upload;
+    } catch (error) {
+      console.error('Error uploading file to S3:', error);
+      throw error;
+    }
   };
 
-
   app.put('/upload', upload.single('image'), async (req, res) => {
-    if (!req.file) {
-      res.status(400).send('File not uploaded');
-      return;
-    }
+    try {
+      if (!req.file) {
+        res.status(400).send('File not uploaded');
+        return;
+      }
 
-    const upload = await uploadFileToBucket({ bucketName: 'your-civo-object-store', file: req.file  });
-    res.send(upload);
+      const upload = await uploadFileToBucket({ bucketName: 'your-civo-object-store', file: req.file  });
+      res.send(upload);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      res.status(500).send('Error uploading file');
+    }
   });
 
   app.get('/all', async (req, res) => {
-    const command = new ListObjectsCommand({ Bucket: 'your-civo-object-store' });
-
     try {
-        const { Contents } = await s3Client.send(command);
-        res.send(Contents);
+      const command = new ListObjectsCommand({ Bucket: 'your-civo-object-store' });
+      const { Contents } = await s3Client.send(command);
+      res.send(Contents);
     } catch (err) {
       console.error(err);
+      res.status(500).send('Error retrieving objects');
     }
-  })
+  });
 
-    app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
+  app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
 
     app.listen(port, () => console.log(`This is the beginning of the Node File Upload App`))
